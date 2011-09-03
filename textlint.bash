5315da5beadebb10a1a5cc9c054536d708a8a8bd
Damien Cassou <damien.cassou@gmail.com>
2011-09-03 12:02:33 +0200

    Print an error message when TextLint was not able to create an output

--- textlint.bash
+++ textlint.bash
@@ -22,5 +22,13 @@ echo
 echo "Please wait while TextLint processes your file: it can take some time..."
 echo
 
+rm -f textlint.log
 "$pharovm" -headless "$textlintimage" $(pwd)/textlint.st
-cat textlint.log
+if [[ ! -e textlint.log ]]; then
+    echo Something bad happened!
+    exit 1
+else
+    cat textlint.log
+    exit 0
+fi
+
